1020a22fe712985a2dee2912c0ac0ff9ed8c4a29
马崑翔 <makunxiang@192.168.5.22>
2026-07-09 13:53:00 +0800

    feat: add poststrat planning

--- tests/testthat/helper-fixtures.R
+++ tests/testthat/helper-fixtures.R
@@ -30,3 +30,34 @@ make_weightflow_fixture <- function() {
 
   list(sample = sample, pop = pop, dims = dims, target = target)
 }
+
+make_poststrat_fixture <- function() {
+  dims <- wf_dims(gender = c("female", "male"), age = c("young", "old"))
+  pop <- data.frame(
+    province = "A",
+    gender = c("female", "male", "female", "male"),
+    age = c("young", "young", "old", "old"),
+    count = c(50, 50, 50, 50),
+    stringsAsFactors = FALSE
+  )
+  sample <- data.frame(
+    id = paste0("r", 1:10),
+    province = "A",
+    gender = c(rep("female", 5), rep("male", 5)),
+    age = c(rep("young", 4), "old", rep("young", 4), "old"),
+    stringsAsFactors = FALSE
+  )
+  target <- wf_target_population(
+    pop,
+    key_map = c(gender = "gender", age = "age"),
+    count = "count",
+    dims = dims,
+    by = "province",
+    keep_joint = TRUE
+  )
+  ladder <- wf_collapse_ladder(
+    dims,
+    level1 = list(age = c("young" = "all", "old" = "all"))
+  )
+  list(sample = sample, target = target, dims = dims, ladder = ladder)
+}
